05c0fadc681f30aa3413b66c1ba83139745ec400
RATIU5 <jcmemmott20@outlook.com>
2023-10-05 08:14:19 -0600

    add: public folder and content

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
+	server := http.NewServeMux()
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from the Go program"))
 	})
-	err := http.ListenAndServe(":3333", nil)
+	err := http.ListenAndServe(":3333", server)
 
 	if err == nil {
 		fmt.Println("Error while opening the server")
