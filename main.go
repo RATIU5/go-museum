787b21bd4bb93758fdd7e214836385cca6ea43de
RATIU5 <jcmemmott20@outlook.com>
2023-10-05 08:55:22 -0600

    add: templates

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from a Go program!!!"))
+}
+
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.Write([]byte("Internal Server Error"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	html.Execute(w, "Test")
+}
+
 func main() {
 	server := http.NewServeMux()
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from the Go program"))
-	})
-	err := http.ListenAndServe(":3333", server)
+	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/template", handleTemplate)
 
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
+
+	err := http.ListenAndServe(":3333", server)
 	if err == nil {
 		fmt.Println("Error while opening the server")
 	}
